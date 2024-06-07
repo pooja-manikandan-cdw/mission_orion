9079034a07707bc7a24238670ebc00d05723fd96
Pooja-Manikandann <Pooja.Manikandan@cdw.com>
2024-06-07 11:56:24 +0530

    assignment-4

--- buddys_nickname/index.js
+++ buddys_nickname/index.js
@@ -8,14 +8,6 @@ const app = express();
 app.use(express.json())
 app.use(BUDDY_PATH, buddyRoutes)
 
-app.use((err, req, res, next) => {
-    if (! err) {
-        return next();
-    }
-
-    res.status(500);
-    res.send({'Error': err});
-});
 
 app.listen(8080, () => {
     console.log('App listening at port 8080');
