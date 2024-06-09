7714052129c28881d47d0ccf03f0eafc3fd632a6
Pooja-Manikandann <Pooja.Manikandan@cdw.com>
2024-06-09 19:13:43 +0530

    assignment-2 review comments - 2

--- httpServer/index.js
+++ httpServer/index.js
@@ -9,16 +9,15 @@ http
       }
       if (data) {
         const readData = JSON.parse(data).data
-        const randomColors = generateRandomColorPalette(readData, 5, readData.length-1)
+        const randomColors = generateRandomColorPalette(readData, 5)
         fs.writeFile("randomized_color_palette.json", JSON.stringify(randomColors), (err) => {
           if (err) {
             console.log("error in writing", err);
           } else {
               console.log("saved...");
               const result =  fs.readFileSync("randomized_color_palette.json", "utf8")
-              console.log('result', result)
-              res.writeHead(200, {'Content-Type': 'text/html'});
-              res.write(result)
+              res.writeHead(200, {'Content-Type': 'json'});
+              res.end(result)
           }
         });
       }
@@ -36,13 +35,14 @@ http
  * @param {number} max max limit of the colors array
  * @returns randomized array with the data received
  */
-function generateRandomColorPalette(data, quantity, max) {
-  const result = [];
-  while(result.length < quantity) {
+function generateRandomColorPalette(data, quantity) {
+  let result = [];
+  let max = data.length-1;
+  for(let i=0;i<quantity; i++) {
     const index = Math.floor(Math.random() * max) + 1
-    if(!result.includes(data[index])) {
       result.push(data[index]);
-    }
+      data=[...data.slice(0, index), ...data.slice(index+1)];
+      max--;
   }
   return(result);
 }
