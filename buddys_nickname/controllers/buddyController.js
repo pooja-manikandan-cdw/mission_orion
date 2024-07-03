491a7e4618918a976da138ebd1b93747d5277a3c
Pooja-Manikandann <Pooja.Manikandan@cdw.com>
2024-07-03 09:45:23 +0530

    Review comments - 2

--- buddys_nickname/controllers/buddyController.js
+++ buddys_nickname/controllers/buddyController.js
@@ -8,23 +8,59 @@ const {
 } = require("../services/buddyServices");
 
 const getAllBuddiesController = (req, res) => {
-  getAllBuddies(req, res);
+  const data = getAllBuddies(req, res);
+  res.send({
+    status: 200,
+    data,
+  });
 };
 
 const getBuddyController = (req, res) => {
-  getBuddy(req, res);
+  const data = getBuddy(req, res);
+  if(data) {
+    res.send({
+      status: 200,
+      data: data,
+    });
+  } else if(!data) {
+    res.status(200).send({
+      status: 204,
+      message: 'Buddy not found',
+    });
+  }
 };
 
 const addBuddyController = (req, res) => {
-  addBuddy(req, res);
+  const data = addBuddy(req, res);
+  if(data) {
+    res.status(400).send({ status: 400, message: "Buddy already exist" });
+  } else {
+    res.status(200).send({ status: 201, message: "Buddy added successfully" });
+  }
 };
 
 const updateBuddyController = (req, res) => {
-  updateBuddy(req, res);
+  const data = updateBuddy(req, res);
+  if(data>=0) {
+    res.status(200).send({ status: 200, message: "Buddy updated successfully" });
+  } else {
+    res.status(200).send({
+      status: 204,
+      message: 'Buddy not found',
+    });
+  }
 };
 
 const deleteBuddyController = (req, res) => {
-  deleteBuddy(req, res);
+  const data = deleteBuddy(req, res);
+  if(data>=0) {
+    res.status(200).send({ status: 200, message: "Buddy deleted successfully" });
+  } else {
+    res.status(200).send({
+      status: 204,
+      message: 'Buddy not found',
+    });
+  }
 };
 
 module.exports = {
--- buddys_nickname/services/buddyServices.js
+++ buddys_nickname/services/buddyServices.js
@@ -5,40 +5,26 @@ const {findItem} = require("../utils/dataHelper");
 
 const getAllBuddies = (req, res) => {
   const data = JSON.parse(readFile(BUDDY_JSON_FILENAME))?.data;
-  res.send({
-    status: 200,
-    data,
-  });
+  return data;
+  
 };
 
 const getBuddy = (req, res) => {
   const employeeId = req.params.employeeId;
   const data = JSON.parse(readFile(BUDDY_JSON_FILENAME))?.data;
   const buddy = findItem(data, Number(employeeId), "employeeId");
-  if(buddy) {
-    res.send({
-      status: 200,
-      data: buddy,
-    });
-  } else if(!buddy) {
-    res.status(200).send({
-      status: 204,
-      message: 'Buddy not found',
-    });
-
-  }
+  return buddy;
 };
 
 const addBuddy = (req, res) => {
   const buddy = req.body;
   const data = JSON.parse(readFile(BUDDY_JSON_FILENAME))?.data;
-  if(!findItem(data, buddy.employeeId, "employeeId")) {
+  const buddyFound = findItem(data, buddy.employeeId, "employeeId")
+  if(!buddyFound) {
     data.push(buddy);
     const response = writeFile(BUDDY_JSON_FILENAME, { data: data })
-    res.status(200).send({ status: 201, message: "Buddy added successfully" });
-  } else {
-    res.status(400).send({ status: 400, message: "Buddy already exist" });
   }
+  return buddyFound;
 };
 
 const updateBuddy = (req, res) => {
@@ -50,13 +36,8 @@ const updateBuddy = (req, res) => {
       const updatesBuddyDetails = {...data[index], ...buddy}
       data = [...data.slice(0, index), {...updatesBuddyDetails}, ...data.slice(index + 1)];
       const response = writeFile(BUDDY_JSON_FILENAME, { data: data })
-      res.status(200).send({ status: 200, message: "Buddy updated successfully" });
-    } else if(index === -1) {
-      res.status(200).send({
-        status: 204,
-        message: 'Buddy not found',
-      });
     }
+    return index;
  };
 
 const deleteBuddy = (req, res) => {
@@ -66,13 +47,8 @@ const deleteBuddy = (req, res) => {
   if(index>=0) {
     data = [...data.slice(0, index), ...data.slice(index + 1)];
     const response = writeFile(BUDDY_JSON_FILENAME, { data: data })
-    res.status(200).send({ status: 200, message: "Buddy deleted successfully" });
-  } else if(index === -1) {
-    res.status(200).send({
-      status: 204,
-      message: 'Buddy not found',
-    });
   }
+  return index;
 };
 
 module.exports = {
